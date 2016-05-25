684404b2d5571371bf6b30d837b832a4fd6d5491
Greg Hale <imalsogreg@gmail.com>
2016-05-25 13:36:58 -0400

    Progress on scaledImage

--- petersonfaces-frontend/src/Main.hs
+++ petersonfaces-frontend/src/Main.hs
@@ -1,10 +1,16 @@
 {-# language RankNTypes          #-}
 {-# language ScopedTypeVariables #-}
+{-# language RecursiveDo         #-}
+{-# language LambdaCase          #-}
 
 module Main where
 
+import Control.Applicative (liftA2)
+import Control.Monad (liftM2)
+import Data.Bool
 import FacesWidget
 import Canvas2D
+import Text.Read
 import Reflex
 import Reflex.Dom
 import Thumbnail
@@ -14,12 +20,49 @@ main = mainWidget run
 
 run :: forall t m.MonadWidget t m => m ()
 run = do
-  t  <- fmap value $ textInput def
+  imgSrc <- stringInput "img  src"
+
+  scale <- doubleInput "scale"
+
+  offset :: Dynamic t (Maybe (Double,Double)) <- liftA2 (,)
+    `mapWidget` doubleInput "x offset"
+    `apWidget`  doubleInput "y offset"
+
+  si <- scaledImage def { sicSetSource = fmapMaybe id $ updated imgSrc
+                        , sicSetScale  = fmapMaybe id $ updated scale
+                        , sicSetOffset = fmapMaybe id $ updated offset}
+  -- let clks :: Event t (Int,Int) = domEvent Mousedown (siEl si)
+  --     clkInfo = attachWith ($) (current $ siNaturalCoords si) clks
   el "br" (return ())
-  elAttr "span" ("style" =: "width:100px;height:100px;background-color:blue;") (text "span")
-  si <- scaledImage (ScaledImageConfig t (constDyn mempty))
-  let clks :: Event t (Int,Int) = domEvent Mousedown (siEl si)
-      clkInfo = attachWith ($) (current $ siNaturalCoords si) clks
-  el "br" (return ())
-  dynText =<< holdDyn "No clicks" (fmap show clkInfo)
+  -- dynText =<< holdDyn "No clicks" (fmap show clkInfo)
   return ()
+
+stringInput :: MonadWidget t m => String -> m (Dynamic t (Maybe String))
+stringInput str = do
+  text str
+  rec t <- snd <$> elDynAttr' "div" divAttrs (value <$> textInput def)
+      v <- forDyn t $ \tx -> bool (Just tx) Nothing (null tx)
+      divAttrs <- forDyn t $ bool mempty ("style" =: "background-color:rgba(255,0,0,0.1);") . null
+  return v
+
+
+doubleInput :: MonadWidget t m => String -> m (Dynamic t (Maybe Double))
+doubleInput str = do
+  text str
+  rec t <- snd <$> elDynAttr' "div" divAttrs (value <$> textInput def)
+      d <- mapDyn readMaybe t
+      divAttrs <- forDyn d $ \case
+        Nothing -> "style" =: "background-color:rgba(255,0,0,0.1);"
+        Just _  -> mempty
+  return d
+
+mapWidget :: MonadWidget t m => (a -> b) -> m (Dynamic t a) -> m (Dynamic t b)
+mapWidget f mw = do
+  w <- mw
+  mapDyn f w
+
+apWidget :: MonadWidget t m => m (Dynamic t (a -> b)) -> m (Dynamic t a) -> m (Dynamic t b)
+apWidget mf ma = do
+  f <- mf
+  a <- ma
+  combineDyn ($) f a
--- petersonfaces-frontend/src/Thumbnail.hs
+++ petersonfaces-frontend/src/Thumbnail.hs
@@ -3,27 +3,29 @@
 {-# language KindSignatures #-}
 {-# language RankNTypes #-}
 {-# language TypeFamilies #-}
+{-# language ScopedTypeVariables #-}
 
 module Thumbnail where
 
-import Control.Monad.IO.Class (MonadIO, liftIO)
-import Data.Map (Map)
-import qualified Data.Map as Map
-import Data.Monoid ((<>))
-import Reflex.Dom hiding (restore)
+import           Control.Monad            (liftM2)
+import           Control.Monad.IO.Class   (MonadIO, liftIO)
+import           Data.Default             (Default, def)
+import           Data.Map                 (Map)
+import qualified Data.Map                 as Map
+import           Data.Monoid              ((<>))
+import           Reflex.Dom               hiding (restore)
 #ifdef ghcjs_HOST_OS
-import GHCJS.DOM.HTMLCanvasElement (getContext, castToHTMLCanvasElement)
+import GHCJS.DOM.HTMLCanvasElement        (getContext, castToHTMLCanvasElement)
 import GHCJS.DOM.CanvasRenderingContext2D (CanvasRenderingContext2D, save, restore, getImageData)
-import GHCJS.DOM.Types (ImageData, Nullable, nullableToMaybe)
-import GHCJS.Marshal (fromJSValUnchecked, toJSVal)
-import GHCJS.DOM.Element (getBoundingClientRect, getClientTop, getClientLeft)
-import           GHCJS.DOM.ClientRect (getTop, getLeft)
+import           GHCJS.DOM.Types          (ImageData, Nullable, nullableToMaybe)
+import           GHCJS.Marshal            (fromJSValUnchecked, toJSVal)
+import           GHCJS.DOM.Element        (getBoundingClientRect, getClientTop, getClientLeft)
+import           GHCJS.DOM.ClientRect     (getTop, getLeft)
 #endif
-import GHCJS.DOM.Types (IsGObject, HTMLCanvasElement, HTMLImageElement)
-import GHCJS.DOM.CanvasRenderingContext2D
-import GHCJS.DOM.HTMLImageElement
-import GHCJS.DOM.Element (getClientTop, getClientLeft)
--- import GHCJS.DOM.ImageData
+import           GHCJS.DOM.Types          (IsGObject, HTMLCanvasElement, HTMLImageElement)
+import           GHCJS.DOM.CanvasRenderingContext2D
+import           GHCJS.DOM.HTMLImageElement
+import           GHCJS.DOM.Element        (getClientTop, getClientLeft)
 
 data ZoomPos = ZoomPos
   { zpZoom    :: Double
@@ -53,6 +55,9 @@ data Thumbnail t = Thumbnail
   }
 
 
+-- | A complicated widget used for panning, zooming, and region-selecting within an img
+--   Movement is achieved through clicks and mousewheels in a 'picture-in-picture' view
+--   in the corner of the widget
 thumbnail :: MonadWidget t m => ThumbnailConfig t -> m (Thumbnail t)
 thumbnail (ThumbnailConfig srcImg attrs dZoom dBB) =
   divClass "thumbnail-widget" $ do
@@ -60,43 +65,112 @@ thumbnail (ThumbnailConfig srcImg attrs dZoom dBB) =
     elDynAttr "div" widgetAttrs $ do
       undefined
 
-data ScaledImageConfig t = ScaledImageConfig
-  { sicSource :: Dynamic t String
-  , sicAttributes :: Dynamic t (Map String String)
+-- | Crop structure contains Ints as a reminder that croppind
+--   is always in pixel units of the original image
+data Crop = Crop
+  { cropLeft   :: Int
+  , cropTop    :: Int
+  , cropRight  :: Int
+  , cropBottom :: Int
   }
+
+instance Default Crop where
+  def = Crop 0 0 0 0
+
+data ScaledImageConfig t = ScaledImageConfig
+  { sicInitialSource :: String
+  , sicSetSource :: Event t String
+  , sicAttributes :: Dynamic t (Map String String)
+  , sicInitialOffset :: (Double, Double)
+  , sicSetOffset :: Event t (Double, Double)
+  , sicInitialScale :: Double
+  , sicSetScale :: Event t Double
+  , sicInitialCrop :: Crop
+  , sicSetCrop :: Event t Crop
+  }
+
+instance Reflex t => Default (ScaledImageConfig t) where
+  def = ScaledImageConfig "" never (constDyn mempty) (0,0) never 1 never def never
+
 
 data ScaledImage t = ScaledImage
   { siImage         :: HTMLImageElement
   , siEl            :: El t
-  , siNaturalCoords :: Dynamic t ((Int,Int) -> (Double,Double))
+  , siNaturalCoords :: Dynamic t ((Int,Int) -> (Double,Double)) -- TODO Don't like this
   }
 
 
+-- | A widget supporting clipping, zooming, and translation of a source image.
+--   Composed of
+--     - a parent div fixed to the size of the source image,
+--     - an intermiate div used as a clipping box
+--     - the source image
 scaledImage :: MonadWidget t m => ScaledImageConfig t -> m (ScaledImage t)
-scaledImage (ScaledImageConfig srcImg attrs) = do
+scaledImage (ScaledImageConfig img0 dImg attrs trans0 dTrans scale0 dScale crop0 dCrop) = mdo
   pb <- getPostBuild
-  imgAttrs <- combineDyn (\s a -> a <> "class" =: "scaled-image" <> "src" =: s) srcImg attrs
-  (resized, imgEl) <- resizeDetector $ fst <$> elDynAttr' "img" imgAttrs (return ())
-  let img = castToHTMLImageElement (_el_element imgEl)
-      imgEvents = leftmost [pb, resized]
-      fI = fromIntegral :: Int -> Double
-  imgInfo <- performEvent $ ffor imgEvents $ \() -> liftIO $ do
-    -- Just rect   <- getBoundingClientRect (_el_element imgEl)
-    -- x      <- floor <$> getLeft rect
-    -- y      <- floor <$> getTop rect
-    widNat <- getNaturalWidth img
-    heiNat <- getNaturalHeight img
-    wid    <- getWidth img
-    hei    <- getHeight img
-    return ((widNat,heiNat),(wid,hei))
-    -- return ((x,y), (widNat,heiNat),(wid,hei))
-  dynText =<< holdDyn "" (fmap show imgInfo)
-  mouseCoordsFn <- holdDyn (\(x,y) -> (fI x, fI y)) $ ffor imgInfo $
-    \((widNat,heiNat),(wid,hei)) (mX,mY) ->
-    let x' = fI mX * fI widNat / fI wid
-        y' = fI mY * fI heiNat / fI hei
-    in  (x',y')
-  return $ ScaledImage img imgEl mouseCoordsFn
+
+  postImg <- delay 0 (leftmost [pb, () <$ dImg])
+  naturalSize :: Dynamic t (Int,Int) <- holdDyn (1 :: Int, 1 :: Int) =<<
+    performEvent (ffor (domEvent Load img) $ \() ->
+                   (,) <$> (getNaturalWidth htmlImg) <*> (getNaturalHeight htmlImg))
+
+  display naturalSize
+
+  let htmlImg = castToHTMLImageElement (_el_element img)
+
+  imgSrc <- holdDyn img0   dImg
+  crop   <- holdDyn crop0  dCrop
+  trans  <- holdDyn trans0 dTrans
+  scale  <- holdDyn scale0 dScale
+
+  clippingDivAttrs <- mkClippingDivAttrs
+
+  parentAttrs <- mkParentAttrs `mapDyn` naturalSize `apDyn` boxSize
+
+  (resized, (parentDiv, (_,img))) <- resizeDetector $ elDynAttr' "div" parentAttrs $ do
+
+    (clipDiv, img) <- elDynAttr' "div" clippingDivAttrs $ do
+
+      imgAttrs <- mkImgAttrs `mapDyn` imgSrc `apDyn` naturalSize `apDyn` scale `apDyn` trans
+      img <- fst <$> elDynAttr' "img" imgAttrs (return ())
+      return img
+
+    return (clipDiv, img)
+
+  boxSize <- return naturalSize -- TODO totally wrong
+
+  return $ ScaledImage htmlImg parentDiv (constDyn (const (1,1)))
+
+  where
+    mkParentAttrs (naturalWid, naturalHei) boxSize =
+        "class" =: "scaled-image-top"
+     <> "style" =: ("overflow:hidden;width:" ++ show naturalWid ++ "px;height:" ++ show naturalHei ++ "px;")
+    mkClippingDivAttrs  = return $ constDyn mempty
+    mkImgAttrs src (naturalWid, naturalHei) scale (offX, offY) =
+      let w :: Int = floor $ fromIntegral naturalWid * scale
+          x :: Int = floor offX
+          y :: Int = floor offY
+      in  "src"   =: src
+       <> "style" =: ("width:" ++ show w ++ "px;position:absolute;top:" ++ show y ++ "px;left:" ++  show x ++ "px;")
+
+  -- imgAttrs <- combineDyn (\s a -> a <> "class" =: "scaled-image" <> "src" =: s) srcImg attrs
+  -- (resized, imgEl) <- resizeDetector $ fst <$> elDynAttr' "img" imgAttrs (return ())
+  -- let img = castToHTMLImageElement (_el_element imgEl)
+  --     imgEvents = leftmost [pb, resized]
+  --     fI = fromIntegral :: Int -> Double
+  -- imgInfo <- performEvent $ ffor imgEvents $ \() -> liftIO $ do
+  --   widNat <- getNaturalWidth img
+  --   heiNat <- getNaturalHeight img
+  --   wid    <- getWidth img
+  --   hei    <- getHeight img
+  --   return ((widNat,heiNat),(wid,hei))
+  -- dynText =<< holdDyn "" (fmap show imgInfo)
+  -- mouseCoordsFn <- holdDyn (\(x,y) -> (fI x, fI y)) $ ffor imgInfo $
+  --   \((widNat,heiNat),(wid,hei)) (mX,mY) ->
+  --   let x' = fI mX * fI widNat / fI wid
+  --       y' = fI mY * fI heiNat / fI hei
+  --   in  (x',y')
+  -- return $ ScaledImage img imgEl mouseCoordsFn
 
 
 #ifndef ghcjs_HOST_OS
@@ -133,3 +207,8 @@ getLeft :: MonadIO m => ClientRect -> m Float
 getLeft = error "getLeft only available in ghcjs"
 
 #endif
+
+apDyn :: MonadWidget t m => m (Dynamic t (a -> b)) -> Dynamic t a -> m (Dynamic t b)
+apDyn mf a = do
+  f <- mf
+  combineDyn ($) f a
